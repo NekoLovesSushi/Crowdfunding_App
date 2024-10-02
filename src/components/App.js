0ae55116eabf226f8c50eb0b2a11122491e84ea4
Eleanor <eleanor.stein777@gmail.com>
2024-10-02 01:23:27 -0400

    9. Create Website

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// Components
+import Navigation from './Navigation';
+import Buy from './Buy';
+import Progress from './Progress';
+import Info from './Info';
+import Loading from './Loading';
+function App() {
+    return(
+
+    )
+}
+export default App;
